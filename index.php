5edc34796aa7edae7df2b1de556e55a3d3197c79
Thomas Müller <thomas@luchsohr.de>
2020-10-22 10:23:20 +0200

    Added Title Tage

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel=stylesheet type="text/css" href="css/format.css">
+<title>PHP Test Page</title>
 </head>
 
 <?php include "textauswertung.php";  ?>
